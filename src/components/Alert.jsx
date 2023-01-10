b25ec4eaa52e1e706d47a51898880404ae3a918e
Revati <revati.v.bordawekar@gmail.com>
2023-01-10 11:36:37 -0800

    added components and basic style template

---
+++ src/components/Alert.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Alert = () => {
+  return (
+    <h2>Alert</h2>
+  )
+}
+
+export default Alert
---
+++ src/components/List.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {FaEdit, FaTrash} from 'react-icons/fa'
+
+const List = () => {
+  return (
+    <h2>List</h2>
+  )
+}
+
+export default List
