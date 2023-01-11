f19ecaf143de78a1c24f052c883f8c218398f140
Revati <revati.v.bordawekar@gmail.com>
2023-01-10 22:33:37 -0800

    remove item logic added

--- src/App.js
+++ src/App.js
@@ -36,6 +36,11 @@ function App() {
     showAlert(true, 'danger', 'List empty');
     setList([]);
   }
+
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter((item) => item.id !== id))
+  }
   return (
     <section className="section-center">
       <form className='grocery-form' onSubmit={handleSubmit}>
@@ -56,7 +61,7 @@ function App() {
           </div>
       </form>
      { list.length > 0 && (<div className="grocery-container">
-        <List items={list} />
+        <List items={list} removeItem={removeItem} />
         <button className='clear-btn' onClick={clearList}>Clear Items</button>
       </div>) }
 
